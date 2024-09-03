a50472c62ed22fcb6d92586106fae39c31f5be4f
TeamL-Legend <NaNLegendN1@gmail.com>
2024-09-03 17:54:09 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 let container = document.querySelector(`.container`);
 let body = document.querySelector(`body`);
 let text = document.querySelector(`.text`);
+let faq = document.querySelector(`.faq`);
 
 setTimeout(() => {
    container.classList.remove(`none`);
    body.classList.remove(`whcl`);
    text.classList.add(`none`);
-  }, 5000);
+   faq.classList.add(`none`);
+  }, 4000);
 
 let txtext = document.querySelector(`.txtext`);
 
@@ -21,12 +23,7 @@ setTimeout(() => {
    }, 2000);
 
 setTimeout(() => {
-    txtext.classList.add(`opac`);
-    txtext.classList.remove(`txtext`);
-   }, 3000);
-
-setTimeout(() => {
     txtext.classList.remove(`opac`);
     txtext.classList.add(`txtext`);
     txtext.innerHTML = `Подключено`;
-   }, 4000);
+   }, 3000);
