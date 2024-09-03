364df3b29021f624d012cca6e2ecda749ea52ed6
TeamL-Legend <NaNLegendN1@gmail.com>
2024-09-03 18:24:27 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@ let container = document.querySelector(`.container`);
 let body = document.querySelector(`body`);
 let text = document.querySelector(`.text`);
 let faq = document.querySelector(`.faq`);
+let trcontainer = document.querySelector(`.trcontainer`);
 
 setTimeout(() => {
    container.classList.remove(`none`);
    body.classList.remove(`whcl`);
    text.classList.add(`none`);
    faq.classList.add(`none`);
+   trcontainer.classList.remove(`none`);
   }, 4000);
 
 let txtext = document.querySelector(`.txtext`);
